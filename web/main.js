92842470da3ab700a1f32cfe34321374e46fc9bd
Alexsander-ETEC <131073044+Alexsander-ETEC@users.noreply.github.com>
2023-06-01 06:01:15 -0300

    Update main.js

--- web/main.js
+++ web/main.js
@@ -1,3 +1,3 @@
 //addEventListener('DOMContentLoaded', function(){document.getElementsByTagName('h1')[0].textContent+=' olaa'});
 //addEventListener('DOMContentLoaded',(e)=>{console.log(e)})
-document.getElementsByTagName('h1')[0].textContent+=' olaa'
+//document.getElementsByTagName('h1')[0].textContent+=' olaa'
