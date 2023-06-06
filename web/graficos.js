d3504a9515dba29e67a109155922201b3c538fb7
Alexsander <131073044+Alexsander-ETEC@users.noreply.github.com>
2023-06-05 21:05:35 -0300

    Delete web directory

--- web/graficos.js
+++
@@ -1,1 +0,0 @@
-const canvas = document.getElementsByTagName('canvas'), c = canvas.getContext('2d');
--- web/main.js
+++
@@ -1,3 +0,0 @@
-//addEventListener('DOMContentLoaded', function(){document.getElementsByTagName('h1')[0].textContent+=' olaa'});
-//addEventListener('DOMContentLoaded',(e)=>{console.log(e)})
-//document.getElementsByTagName('h1')[0].textContent+=' olaa'
