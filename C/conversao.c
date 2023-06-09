0bda8f0287d81d59ab3ead69270d21382e91da7c
Alexsander <131073044+Alexsander-ETEC@users.noreply.github.com>
2023-06-09 20:36:54 -0300

    Add files via upload

--- C/conversao.c
+++ C/conversao.c
@@ -4,10 +4,15 @@ main() {
 	char continuar = 's';
 	while(continuar == 's') {
 		float valor, cotacao, total;
-		printf("Digite o valor a ser convertido: "); scanf("%f", &valor);
-		printf("Digite o valor da cotacao do dolar: "); scanf("%f", &cotacao);
-		total = valor * cotacao;
-		printf("O valor convertido corresponde a: %.2f", total);
-		printf("\nDeseja continuar? [s/n] "); scanf("%s", &continuar);
+		printf("Digite o valor a ser convertido em reais: $ "); scanf("%f", &valor);
+		if (valor < 10) {
+			printf("Nao pode converter um valor menor que $10 dolares");
+		} else {
+			printf("Digite o valor da cotacao do dolar: $ "); scanf("%f", &cotacao);
+			total = valor * cotacao;
+			printf("O valor convertido corresponde a: R$ %.2f", total);
+		}
+		printf("\nDeseja fazer outra conversao? [s/n] "); scanf("%s", &continuar);
+		printf("\n");
 	}
 }
