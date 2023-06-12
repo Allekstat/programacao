34de2ee6bea26632a122293bb7cfd43773746d9f
Alexsander <131073044+Alexsander-ETEC@users.noreply.github.com>
2023-06-12 19:53:27 -0300

    Update ASCII.c

--- C/ASCII.c
+++ C/ASCII.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 int main(int arguments_count, char* arguments_array[]) {
-	int i;
-	for(i = 0; i < 256; i++) {
-		char ascii = i;
-		printf("char[%i]  = \"%c\";\n", i, ascii);
+	if(arguments_count > 1) {
+		if (strcmp(arguments_array[1], "list") == 0) {
+			int i;
+			for(i = 0; i < 256; i++) {
+				char ascii = i;
+				printf("char[%i]  = \"%c\";\n", i, ascii);
+			}
+		}
 	}
 	return 0;
 }
