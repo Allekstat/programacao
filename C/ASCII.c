5c1bcbbbd479beeae292f06b67cfcb6c4904740f
Alexsander <131073044+Alexsander-ETEC@users.noreply.github.com>
2023-06-21 07:05:25 -0300

    Update ASCII.c

--- C/ASCII.c
+++ C/ASCII.c
@@ -3,44 +3,57 @@
 #include <string.h>
 
 char* comandos[] = {"sair", "help", "listar"};
+
 char* descricoes[] = {
 	"Finaliza o programa.",
 	"Exibe todos os comandos disponíveis.",
 	"Lista todos os caracteres ASCII de 0 a 255."
 };
+
 int comando_existe(char* comando) {
 	int i;
 	for(i = 0; i < sizeof(comandos); i++) {
-		if(strcmp(comandos[i], input)) {
-			return 1;
+		if(strcmp(comandos[i], comando) == 0) {
+			if(i < 1) {
+				return i - 1;
+			} else {
+				return i;
+			}
 		}
 	}
 	return 0;
 }
-int comando_indice() {
-	
-}
-int checar(char* comando) {
-	if (strcmp(comando, "help") == 0) {
-		return 1;
-	} else if (strcmp(comando, "listar") == 0) {
-		return 2;
-	} else if (strcmp(comando, "sair") == 0) {
-		return -1;
-	} else {
-		return 0;
-	}
-}
-void funcao(int codigo) {
+
+// int comando_indice() {}
+
+// int checar(char* comando) {
+// 	if (strcmp(comando, "sair") == 0) {
+// 		return -1;
+// 	} else if (strcmp(comando, "help") == 0) {
+// 		return 1;
+// 	} else if (strcmp(comando, "listar") == 0) {
+// 		return 2;
+// 	} else {
+// 		return 0;
+// 	}
+// }
+
+void comando_funcao(int codigo) {
 	int i;
 	switch(codigo) {
+		case -1:
+			exit();
+			break;
 		case 1:
-			
+			for(i = 0; i < sizeof(comandos); i++) {
+				printf(" < %s", comandos[i]);
+				printf(" \t\t\t %s", descricoes[i]);
+			}
 			break;
 		case 2:
 			for(i = 0; i < 256; i++) {
 				char ascii = i;
-				printf("char[%i]  = \"%c\";\n", i, ascii);
+				printf(" < char[%i]  = \"%c\";\n", i, ascii);
 			}
 			break;
 		default:;
@@ -48,12 +61,12 @@ void funcao(int codigo) {
 }
 int main(int arguments_count, char* arguments_array[]) {
 	if(arguments_count > 1) {
-		funcao(checar(arguments_array[1]));
+		comando_funcao(comando_existe(arguments_array[1]));
 	} else {
 		char input[16];
-		while(checar(input) != -1) {
+		while(comando_existe(input) != -1) {
 			printf(" > "); scanf("%s", &input);
-			funcao(checar(input));
+			comando_funcao(comando_existe(input));
 		}
 	}
 	return 0;
