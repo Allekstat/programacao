08f9e09583888b04f6c0ea33b9d8cfa3b4c93c4f
Alexsander <131073044+Alexsander-ETEC@users.noreply.github.com>
2023-10-09 20:39:37 -0300

    Add files via upload

---
+++ tabela.sql
@@ -0,0 +1,54 @@
+DROP DATABASE IF EXISTS clima;
+CREATE DATABASE clima;
+USE clima;
+
+CREATE TABLE registro(
+	id INT NOT NULL AUTO_INCREMENT,
+
+	data DATE NOT NULL,
+	temperatura DECIMAL(4,2) NOT NULL,
+	umidade DECIMAL(4,2) NOT NULL,
+
+	PRIMARY KEY(id)
+);
+
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 25, 89);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 25, 89);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 25, 89);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 25, 89);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 25, 89);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 25, 89);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 25, 89);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 25, 89);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 25, 89);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 25, 89);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 18.1, 65);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 18.1, 65);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 18.1, 65);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 18.1, 65);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 18.1, 65);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 18.1, 65);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 18.1, 65);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 18.1, 65);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 18.1, 65);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 18.1, 65);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 21, 36);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 21, 36);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 21, 36);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 21, 36);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 21, 36);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 21, 36);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 21, 36);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 21, 36);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 21, 36);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 21, 36);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 19, 90);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 19, 90);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 19, 90);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 19, 90);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 19, 90);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 19, 90);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 19, 90);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 19, 90);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 19, 90);
+INSERT INTO registro(data, temperatura, umidade) values('2023-10-09', 19, 90);
